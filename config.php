dc4bd8917bd3f7314e142e7243e00bc03f4b5daf
Christo Fouche <christo@plettbusiness.co.za>
2013-03-21 22:15:40 +0200

    Config File added

---
+++ config.php
@@ -0,0 +1,69 @@
+<?php
+
+// Database Data
+
+// Set this to match your Database Host
+$dbhost = "localhost";
+
+// Set this to match your Database Username
+$dbuser = "username";
+
+// Set this to match your Database Password
+$dbpassword = "password";
+
+// Set this to match your Database Name
+$dbdatabase = "my_database";
+
+
+// ***** Global Variables *****
+
+// Site URL excluding http://
+// Make sure this is the Directory where your code is located
+// Exclude the trailing /
+
+$_ENV['SITE_ID'] = 'www.mywebsite.com';
+
+// Site URL including http://
+// Make sure this is the Directory where your code is located
+// Exclude the trailing /
+
+$_ENV['SITE_URL'] = 'http://www.mywebsite.com';
+
+// Site Header
+
+$_ENV['SITE_HEADER'] = 'Business Directory';
+
+// Site Title
+
+$_ENV['SITE_NAME'] = 'BDM Demo Business Directory';
+
+// Header Title Tag
+// To be Used in the Title Meta Tags
+
+$_ENV['HEADER_TAG'] = 'Business Direct Media Business Directory';
+
+// Header Description Tag
+// To be Used in the Description Meta Tags
+
+$_ENV['HEADER_TAG'] = 'Business Direct Media | Open Source Business Directory Script';
+
+// Template Name
+// If you dont know what this is, leave it as default
+
+$_ENV['TEMPLATE_NAME'] = 'default';
+
+// Directory Name
+
+$_ENV['DIRECTORY_NAME'] = 'South Africa Business Directory';
+
+// Main Country Name
+
+$_ENV['COUNTRY_NAME'] = 'South Africa';
+
+// Marketing Email Address receives Notifications of Upgrades
+
+$_ENV['MARKETING_EMAIL'] = 'myname@mywebsite.com';
+
+
+?>
+
